58688d24bc9916342758ce8ce20e6751793a696b
Florian Cassayre <florian.cassayre@gmail.com>
2024-06-26 21:44:33 +0200

    Finalize redirections and add legacy static resources

--- src/content/redirect/legacy.ts
+++ src/content/redirect/legacy.ts
@@ -7,8 +7,11 @@ export const REDIRECT_PAGE_NAME = '_redirect';
  */
 export const INVISIBLE_REDIRECTIONS: Record<string, string> = {
   '/gpg': '/pgp',
-  '/projects': '/',
-  '/realisations': '/',
+  '/projects': '/realisations',
+  '/genealogy': '/articles/genealogy',
+  '/articles/genealogy': 'https://genealogy.florian.cassayre.me',
+  '/about': '/',
+  '/netherrail': 'https://zeps.carrade.eu',
 };
 
 const legacyGithubFile = (path: string) => `https://github.com/FlorianCassayre/Website/blob/master/${path}`;
@@ -17,17 +20,43 @@ const legacyGithubTemplate = (template: string) => legacyGithubFile(`templates/$
 /**
  * A redirection is proposed to the user (requires actions from their side).
  */
-const VISIBLE_REDIRECTIONS: [path: RegExp | string, fallback: string][] = [
+const VISIBLE_REDIRECTIONS: [path: RegExp | string | string[], fallback: string][] = [
   ['/donation', legacyGithubTemplate('general/donation')],
   ['/realisations', legacyGithubFile('contents/florian/projects.json')],
   ['/api', legacyGithubTemplate('general/api')],
-  // TODO add more
+  [/^(\/uuid|\/tools\/minecraft\/uuid(\/[^/]*)?)$/, legacyGithubTemplate('tools/uuid')],
+  [['/enchanting', '/tools/minecraft/enchanting'], legacyGithubTemplate('tools/enchanting/homepage')],
+  [
+    /^\/tools\/minecraft\/enchanting(\/combinations(\/[a-z]+\/[a-z]+\/[0-9]+(\/[a-z]+\/(any|[1-5]))?)?)?$/,
+    legacyGithubTemplate('tools/enchanting/combinations'),
+  ],
+  ['/tools/minecraft/enchanting/constraints', legacyGithubTemplate('tools/enchanting/constraints')],
+  [['/ping', '/tools/minecraft/ping'], legacyGithubTemplate('general/not_yet')],
+  [['/files/uhc-model.html', '/ktz/6'], legacyGithubTemplate('static/ktz6')],
+  ['/api', legacyGithubTemplate('general/api')],
+  ['/brand', legacyGithubTemplate('general/brand')],
+  ...['crypto', 'js', 'float', 'random', 'youtube', 'pins', 'genealogy', 'publibike']
+    .map(article => `articles/${article}`)
+    .map(path => [`/${path}`, legacyGithubTemplate(path)] satisfies [string, string]),
+  ['/tools/minecraft/zcraft/tracker', legacyGithubTemplate('tools/ztracker')],
+  [
+    /^\/tools\/minecraft\/zcraft\/tracker\/image\/v6(_(nether|end))?.png$/,
+    legacyGithubFile('src/FlorianCassayre/Florian/Controllers/Tools/ZcraftTrackerController.php'),
+  ],
+  ['/comptebon', legacyGithubTemplate('static/comptebon')],
+  ['/card', legacyGithubTemplate('static/card')],
+  ['/card2', legacyGithubTemplate('static/card2')],
+  // Dropped (old redirections that were deprecated long time ago):
+  // /api/minecraft/netherrail
+  // /api/minecraft/netherrail-list
 ];
 
 export const getLegacyVisibleRedirection = (urlPathname: string): string | null =>
   VISIBLE_REDIRECTIONS.filter(([expr]) => {
     if (typeof expr === 'string') {
       return expr === urlPathname;
+    } else if (Array.isArray(expr)) {
+      return expr.includes(urlPathname);
     } else {
       return expr.exec(urlPathname);
     }
--- src/pages/+Layout.tsx
+++ src/pages/+Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import '@fontsource/inter';
+import '@fontsource/bitter';
 import './Layout.css';
 
 import { CssVarsProvider as JoyCssVarsProvider } from '@mui/joy';
