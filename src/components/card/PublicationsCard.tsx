620bc6a797f7c1949c677ccc7777b560a6ce60ed
Florian Cassayre <florian.cassayre@gmail.com>
2025-01-18 18:06:35 +0100

    Update publications and fix some translations

--- src/components/card/PublicationsCard.tsx
+++ src/components/card/PublicationsCard.tsx
@@ -12,7 +12,7 @@ import {
   Stack,
   Typography,
 } from '@mui/joy';
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 interface Publication {
   title: string;
@@ -20,8 +20,9 @@ interface Publication {
   conference?: string;
   authors: string[];
   context?: string;
-  description: string;
-  link: string;
+  abstract: string;
+  link?: string;
+  upcoming?: boolean;
 }
 
 interface PublicationsCardProps {
@@ -33,36 +34,43 @@ export const PublicationsCard: React.FC<PublicationsCardProps> = ({ publications
   return (
     <Card sx={{ p: 0, overflow: 'hidden' }}>
       <AccordionGroup>
-        {publications.map(({ title, year, conference, authors, context, description, link }, i) => (
-          <Accordion key={i}>
+        {publications.map(({ title, year, conference, authors, context, abstract, link, upcoming }, i) => (
+          <Accordion key={i} disabled={upcoming}>
             <AccordionSummary>
               <Stack direction="row" spacing={1} alignItems="center">
                 <Chip variant="outlined">{year}</Chip>
                 <Typography>{title}</Typography>
                 {!!conference && <Chip color="primary">{conference}</Chip>}
+                {!!upcoming && (
+                  <Chip color="warning">
+                    <FormattedMessage id="home.publications.future" />
+                  </Chip>
+                )}
               </Stack>
             </AccordionSummary>
             <AccordionDetails>
               <Stack direction="row" spacing={1}>
                 <Stack direction="column" spacing={1} sx={{ width: '100%' }}>
                   <Typography level="body-sm">{authors.join(', ')}</Typography>
-                  <Typography textAlign="justify">{description}</Typography>
+                  <Typography textAlign="justify">{abstract}</Typography>
                   {!!context && (
                     <Typography level="body-sm" fontStyle="italic">
                       {context}
                     </Typography>
                   )}
                 </Stack>
-                <IconButton
-                  component="a"
-                  href={link}
-                  target="_blank"
-                  rel="noopener"
-                  sx={{ flexShrink: 0 }}
-                  aria-label={$t({ id: 'common.externalLink' })}
-                >
-                  <Launch color="action" />
-                </IconButton>
+                {!!link && (
+                  <IconButton
+                    component="a"
+                    href={link}
+                    target="_blank"
+                    rel="noopener"
+                    sx={{ flexShrink: 0 }}
+                    aria-label={$t({ id: 'common.externalLink' })}
+                  >
+                    <Launch color="action" />
+                  </IconButton>
+                )}
               </Stack>
             </AccordionDetails>
           </Accordion>
--- src/content/home/card/PublicationsCardContent.tsx
+++ src/content/home/card/PublicationsCardContent.tsx
@@ -12,12 +12,20 @@ export const PublicationsCardContent: React.FC = () => {
     <PublicationsCard
       publications={[
         {
+          title: 'Upgrading the IMPACT Application for Enhanced Risk Declaration and Approval Processes at CERN',
+          year: 2025,
+          conference: "IPAC'25",
+          authors: [],
+          abstract: '',
+          upcoming: true,
+        },
+        {
           title: 'LISA: Towards a Foundational Theorem Prover',
           year: 2022,
           conference: "AITP'22",
           authors: ['Simon Guilloud', 'Florian Cassayre', 'Viktor Kunčak'],
-          description:
-            'We present the foundations and initial implementation of a new interactive theorem prover, named LISA. [...] LISA aims to use classical mainstream foundations of mathematics [...]. LISA uses (single-sorted) first order logic (with schematic variables) as the syntactic framework and set theory axiom schemas as the semantic framework. [...] On top of these foundations we can construct numbers and other mathematical theories and models of computation without introducing new axioms. As the target use of LISA we envision mathematical statements as well as formal proofs of computer programs and systems [...]. An important aim of LISA is interoperability with other proof assistants. We hope that the design of LISA with small, fresh code base, simple foundation and explicit proof objects will encourage building bridges with other tools.',
+          abstract:
+            'We present the foundations and initial implementation of a new interactive theorem prover, named LISA. In a slight contrast to most popular type-theoretic frameworks, and much like Mizar, LISA aims to use classical mainstream foundations of mathematics, taking a hint, among others from the talk of John Harrison in this very venue in 2018. LISA uses (single-sorted) first order logic (with schematic variables) as the syntactic framework and set theory axiom schemas as the semantic framework. On top of these foundations we can construct numbers and other mathematical theories and models of computation without introducing new axioms. As the target use of LISA we envision mathematical statements as well as formal proofs of computer programs and systems, possibly with probabilistic and distributed behavior. For automation in LISA we expect to employ newly developed algorithms for equivalence checking of formulas and proofs, existing high-performance superposition-based theorem provers such as Vampire, SPASS, E and Zipperposition, as well as SMT solvers such as Z3, CVC5, and veriT, and OpenSMT. An important aim of LISA is interoperability with other proof assistants. We hope that the design of LISA with small, fresh code base, simple foundation and explicit proof objects will encourage building bridges with other tools. We also expect that the system will serve as a good vehicle to explore machine-learning guided superposition proof search, with learned heuristics complementing hand-tuned ones. We also plan to explore high-level translation of proofs from other systems into LISA, inspired by the success of deep neural networks in natural language translation.',
           link: 'https://aitp-conference.org/2022/abstract/AITP_2022_paper_23.pdf',
         },
         {
@@ -25,8 +33,8 @@ export const PublicationsCardContent: React.FC = () => {
           year: 2022,
           conference: "EI'22",
           authors: ['Qiyuan Liang', 'Florian Cassayre', 'Haley Owsianko', 'Majed El Helou', 'Sabine Süsstrunk'],
-          description:
-            'Deep image denoisers achieve state-of-the-art results but with a hidden cost. As witnessed in recent literature, these deep networks are capable of overfitting their training distributions, causing inaccurate hallucinations to be added to the output and generalizing poorly to varying data. [...] We propose a novel framework exploiting a denoising network. We call it controllable confidence-based image denoising (CCID). [...] We exploit the outputs of a deep denoising network alongside an image convolved with a reliable filter. [...] We propose to fuse the two components with a frequency-domain approach that takes into account the reliability of the deep network outputs. [...]. Our CCID not only provides more interpretability and control, but can even outperform both the quantitative performance of the deep denoiser and that of the reliable filter, especially when the test data diverge from the training data.',
+          abstract:
+            'Deep image denoisers achieve state-of-the-art results but with a hidden cost. As witnessed in recent literature, these deep networks are capable of overfitting their training distributions, causing inaccurate hallucinations to be added to the output and generalizing poorly to varying data. For better control and interpretability over a deep denoiser, we propose a novel framework exploiting a denoising network. We call it controllable confidence-based image denoising (CCID). In this framework, we exploit the outputs of a deep denoising network alongside an image convolved with a reliable filter. Such a filter can be a simple convolution kernel which does not risk adding hallucinated information. We propose to fuse the two components with a frequency-domain approach that takes into account the reliability of the deep network outputs. With our framework, the user can control the fusion of the two components in the frequency domain. We also provide a user-friendly map estimating spatially the confidence in the output that potentially contains network hallucination. Results show that our CCID not only provides more interpretability and control, but can even outperform both the quantitative performance of the deep denoiser and that of the reliable filter, especially when the test data diverge from the training data.',
           link: 'https://arxiv.org/abs/2201.00429',
         },
         {
@@ -34,7 +42,7 @@ export const PublicationsCardContent: React.FC = () => {
           year: 2022,
           authors: ['Florian Cassayre'],
           context: $t({ id: `${Key}.lisaFrontMscThesis.context` }),
-          description: $t({ id: `${Key}.lisaFrontMscThesis.shortAbstract` }),
+          abstract: $t({ id: `${Key}.lisaFrontMscThesis.shortAbstract` }),
           link: 'https://doi.org/10.5281/zenodo.6645113',
         },
         {
@@ -42,7 +50,7 @@ export const PublicationsCardContent: React.FC = () => {
           year: 2020,
           authors: ['Florian Cassayre'],
           context: $t({ id: `${Key}.lcfMscProject.context` }),
-          description:
+          abstract:
             'The goal of this semester project was to design a proof framework and use it to formalize elementary set theory. Safety is ensured by the compiler through type checking, while formal soundness is guaranteed by the execution. This hybrid verification process allows the framework to be used interactively inside an IDE. It is shown how types can encode logic formulas, in particular first order logic and higher order theories.',
           link: 'https://doi.org/10.5281/zenodo.4284080',
         },
