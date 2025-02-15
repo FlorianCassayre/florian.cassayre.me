5c10a7b9a8dd03f12792169e056e8d4fa8d474ed
Florian Cassayre <florian.cassayre@gmail.com>
2025-02-15 20:37:36 +0100

    Fix language button not updating on history change

--- src/components/LanguageToggle.tsx
+++ src/components/LanguageToggle.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { Button, ToggleButtonGroup } from '@mui/joy';
 import { usePageContext } from 'vike-react/usePageContext';
@@ -7,16 +7,13 @@ import { LOCALES, localizeUrl } from '../i18n/utils';
 
 export const LanguageToggle: React.FC = () => {
   const { locale, urlPathname } = usePageContext();
-  // For the 404 page on SSG
-  const [uiLocale, setUiLocale] = useState(locale); // To avoid the flickering effect
   return (
-    <ToggleButtonGroup variant="plain" value={[uiLocale]}>
+    <ToggleButtonGroup variant="plain" value={[locale]}>
       {LOCALES.map(locale => (
         <Button
           key={locale}
           component="a"
           href={localizeUrl(urlPathname, locale)}
-          onClick={() => setUiLocale(locale)}
           {...{ value: locale }}
           sx={{ px: 1 }}
           role="button"
